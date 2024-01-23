6b62c2c4dfb7dab78e1e8c61f3a8b8238e4f3851
chamaahandsome <timboywa@gmail.com>
2024-01-23 08:51:02 -0500

    Update code formatting and fix minor bugs

---
+++ App.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { ThemeProvider } from "styled-components/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { SafeArea } from "./utils/safe-area.component";
+import { DashboardNavigation } from "./src/navigation/dashboard-navigation";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Dashboard: "view-dashboard-outline",
+  Transactions: "credit-card-multiple-outline",
+  Cards: "cards-outline",
+  Wallet: "wallet-outline",
+  Profile: "account-settings-outline",
+};
+
+const Transactions = () => (
+  <SafeArea>
+    <Text>Transactions</Text>
+  </SafeArea>
+);
+
+const Cards = () => (
+  <SafeArea>
+    <Text>cards</Text>
+  </SafeArea>
+);
+
+const Wallet = () => (
+  <SafeArea>
+    <Text>Wallet</Text>
+  </SafeArea>
+);
+
+const Profile = () => (
+  <SafeArea>
+    <Text>Profile</Text>
+  </SafeArea>
+);
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <MaterialCommunityIcons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "#00CED1",
+    tabBarInactiveTintColor: "gray",
+  };
+};
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={createScreenOptions}>
+          <Tab.Screen
+            name="Dashboard"
+            component={DashboardNavigation}
+            // options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Transactions"
+            component={Transactions}
+            // options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Cards"
+            component={Cards}
+            // options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Wallet"
+            component={Wallet}
+            // options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            // options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
